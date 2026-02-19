26502d5c61b8e115d84842e108278bd1bb4ed033
devjmmg <166524587+devjmmg@users.noreply.github.com>
2026-02-19 12:32:36 -0600

    style: Logo y links en color lime

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <h1 class="text-4xl">
-    Dev<span class="font-bold">Jobs</span>
+    Dev<span class="font-bold text-lime-500">Jobs</span>
 </h1>
--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,5 +1,5 @@
 @php
-    $classes = "text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-500";
+    $classes = "text-sm text-gray-600 hover:text-lime-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-500";
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
